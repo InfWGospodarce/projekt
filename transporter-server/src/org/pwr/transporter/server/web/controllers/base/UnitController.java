75050cd57d83876a263f4653185e77b319b6b9d0
r0x0 <x0r@x0r-mob>
2015-01-08 12:23:50 +0100

    add unit type

--- transporter-server/src/org/pwr/transporter/server/web/controllers/base/UnitController.java
+++ transporter-server/src/org/pwr/transporter/server/web/controllers/base/UnitController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.pwr.transporter.entity.base.Unit;
+import org.pwr.transporter.entity.enums.base.UnitDimension;
 import org.pwr.transporter.server.core.hb.criteria.Criteria;
 import org.pwr.transporter.server.web.controllers.GenericController;
 import org.pwr.transporter.server.web.services.UnitService;
@@ -22,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-
 /**
  * <pre>
  *    Controller for {@link Unit}
@@ -35,69 +35,65 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UnitController extends GenericController {
 
-    private static Logger LOGGER = Logger.getLogger(UnitController.class);
+	private static Logger LOGGER = Logger.getLogger(UnitController.class);
 
-    @Autowired
-    UnitService service;
+	@Autowired
+	UnitService service;
 
-    @Autowired
-    UnitValidator validator;
+	@Autowired
+	UnitValidator validator;
 
+	@RequestMapping(value = "/admin/unitList", method = RequestMethod.GET)
+	public String getList( HttpServletRequest request, HttpServletResponse response, Model model ) {
 
-    @RequestMapping(value = "/admin/unitList", method = RequestMethod.GET)
-    public String getList(HttpServletRequest request, HttpServletResponse response, Model model) {
+		Criteria criteria = restoreCriteria(request);
+		List<Unit> list = getListWithCriteria(service, request, criteria);
 
-        Criteria criteria = restoreCriteria(request);
-        List<Unit> list = getListWithCriteria(service, request, criteria);
+		model.addAttribute("list", list);
 
-        model.addAttribute("list", list);
+		return "/Views/admin/unitList";
+	}
 
-        return "/Views/admin/unitList";
-    }
+	@RequestMapping(value = "/admin/unitEdit", method = RequestMethod.GET)
+	public String get( HttpServletRequest request, HttpServletResponse response, Model model ) {
 
+		Long id = getId(request.getParameter("id"));
+		loadData(model);
+		Unit object = null;
+		if ( id == null ) {
+			object = new Unit();
+		} else {
+			object = service.getByID(id);
+			if ( object == null || object.getId() == null ) {
+				object = new Unit();
+			}
+		}
 
-    @RequestMapping(value = "/admin/unitEdit", method = RequestMethod.GET)
-    public String get(HttpServletRequest request, HttpServletResponse response, Model model) {
+		model.addAttribute("object", object);
 
-        Long id = getId(request.getParameter("id"));
-        loadData(model);
-        Unit object = null;
-        if( id == null ) {
-            object = new Unit();
-        } else {
-            object = service.getByID(id);
-            if( object == null || object.getId() == null ) {
-                object = new Unit();
-            }
-        }
+		return "Views/base/unitEdit";
+	}
 
-        model.addAttribute("object", object);
+	@RequestMapping(value = "/admin/unitEdit", method = RequestMethod.POST)
+	public String post( HttpServletRequest request, HttpServletResponse response, @ModelAttribute("object") @Validated Unit object,
+			BindingResult formBindeings, Model model ) {
 
-        return "Views/base/unitEdit";
-    }
+		if ( !validate(object, model, formBindeings, validator) ) {
+			return "/Views/base/unitEdit";
+		}
 
+		if ( object.getId() != null ) {
+			LOGGER.debug("Id not null");
+			service.update(object);
+		} else {
+			service.insert(object);
+		}
 
-    @RequestMapping(value = "/admin/unitEdit", method = RequestMethod.POST)
-    public String post(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("object") @Validated Unit object,
-            BindingResult formBindeings, Model model) {
+		return "redirect:../admin/unitList?page=" + getPage(request);
+	}
 
-        if( !validate(object, model, formBindeings, validator) ) {
-            return "/Views/base/unitEdit";
-        }
-
-        if( object.getId() != null ) {
-            LOGGER.debug("Id not null");
-            service.update(object);
-        } else {
-            service.insert(object);
-        }
-
-        return "redirect:../admin/unitList?page=" + getPage(request);
-    }
-
-
-    @Override
-    public void loadData(Model model) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void loadData( Model model ) {
+		model.addAttribute("dimensions", UnitDimension.values());
+	}
 }
--- transporter-server/src/org/pwr/transporter/server/web/services/UnitService.java
+++ transporter-server/src/org/pwr/transporter/server/web/services/UnitService.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 import org.pwr.transporter.entity.base.Currency;
 import org.pwr.transporter.entity.base.Unit;
+import org.pwr.transporter.entity.enums.base.UnitDimension;
 import org.pwr.transporter.server.business.UnitLogic;
 import org.pwr.transporter.server.core.hb.criteria.Criteria;
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 
 /**
@@ -23,64 +23,60 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class UnitService implements IService {
 
-    @Autowired
-    UnitLogic unitLogic;
+	@Autowired
+	UnitLogic unitLogic;
 
+	public Unit getByID( Long id ) {
 
-    public Unit getByID(Long id) {
-        return this.unitLogic.getByID(id);
-    }
+		Unit unit = this.unitLogic.getByID(id);
+		if ( unit.getUnitType() != null ) {
+			unit.setUnitTypeValue(String.valueOf(unit.getUnitType().getValue()));
+		}
+		return unit;
+	}
 
+	public List<Unit> search( Map<String, Object> parameterMap ) {
+		return this.unitLogic.search(parameterMap);
+	}
 
-    public List<Unit> search(Map<String, Object> parameterMap) {
-        return this.unitLogic.search(parameterMap);
-    }
+	public Long insert( Unit entity ) {
+		entity.setUnitType(UnitDimension.getUnitbyValue(Integer.parseInt(entity.getUnitTypeValue())));
+		return this.unitLogic.insert(entity);
+	}
 
+	public void update( Unit entity ) {
+		entity.setUnitType(UnitDimension.getUnitbyValue(Integer.parseInt(entity.getUnitTypeValue())));
+		this.unitLogic.update(entity);
+	}
 
-    public Long insert(Unit entity) {
-        return this.unitLogic.insert(entity);
-    }
+	public void delete( Unit entity ) {
+		this.unitLogic.delete(entity);
+	}
 
+	public void deleteById( Long id ) {
+		this.unitLogic.deleteById(id);
+	}
 
-    public void update(Unit entity) {
-        this.unitLogic.update(entity);
-    }
+	@SuppressWarnings("unchecked")
+	public List<Unit> getListRest( int amount, int fromRow ) {
+		return this.unitLogic.getListRest(amount, fromRow);
+	}
 
+	public long count() {
+		return this.unitLogic.count();
+	}
 
-    public void delete(Unit entity) {
-        this.unitLogic.delete(entity);
-    }
+	public long count( Criteria criteria ) {
+		return this.unitLogic.count(criteria);
+	}
 
+	@SuppressWarnings("unchecked")
+	public List<Unit> getListRestCrit( int amount, int fromRow, Criteria criteria ) {
+		return this.unitLogic.getListRestCrit(amount, fromRow, criteria);
+	}
 
-    public void deleteById(Long id) {
-        this.unitLogic.deleteById(id);
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public List<Unit> getListRest(int amount, int fromRow) {
-        return this.unitLogic.getListRest(amount, fromRow);
-    }
-
-
-    public long count() {
-        return this.unitLogic.count();
-    }
-
-
-    public long count(Criteria criteria) {
-        return this.unitLogic.count(criteria);
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public List<Unit> getListRestCrit(int amount, int fromRow, Criteria criteria) {
-        return this.unitLogic.getListRestCrit(amount, fromRow, criteria);
-    }
-
-
-    public Object getList() {
-        return this.unitLogic.getList();
-    }
+	public Object getList() {
+		return this.unitLogic.getList();
+	}
 
 }
